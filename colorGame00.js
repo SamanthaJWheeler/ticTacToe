ffcc7f7ff08d9cd83f10235008e021374b6b0865
SamanthaJWheeler <115271437+SamanthaJWheeler@users.noreply.github.com>
2023-03-11 10:28:55 -0500

    Update colorGame00.js

--- colorGame00.js
+++ colorGame00.js
@@ -1,19 +1,34 @@
+const Square = () =>{
+  return (
+    <button> </button>
+  )
+}
+
 const Board = () => {
   const [player, setPlayer] = React.useState(0);
   let status = `Player ${player}`;
+  function renderSquare(i){
+    return <Square> </Square>;
+  }
   return (
-    <div
-      className="game-board"
-      onClick={(e) => {
-        setPlayer(player ? 0:1);
-        status = `Player ${player}`;
-      }}
-    >
-      <div id="info">
+    <div className="game-board">
+        <div className="grid-row">{renderSquare(0)}</div>
+        <div id="info">
         <h1>{status}</h1>
       </div>
     </div>
   );
+  
+  // ==============================
+  //    onClick={(e) => {
+   //     setPlayer(player ? 0:1);
+  //      status = `Player ${player}`;
+  //      e.target.style.background = "green";
+  //      e.target.style.width = 600;
+  //    }}
+  //  >
+
+ // );
 };
 
 // ========================================
