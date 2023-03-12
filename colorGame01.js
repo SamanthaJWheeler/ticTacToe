0a6b018df39f96cdc106a3e1e2c5213c07409cf0
SamanthaJWheeler <115271437+SamanthaJWheeler@users.noreply.github.com>
2023-03-12 15:03:17 -0400

    edits

--- colorGame01.js
+++ colorGame01.js
@@ -1,5 +1,7 @@
-const Square = ({id, player, newState}) => {
+const Square = ({id, newState}) => {
   const [color, setColor] = React.useState('green');
+  const [status, setStatus] = React.useState(null);
+  const xo = ["O","X"];
   const palet = ["red", "blue", "green"];
   const getRandomColor = () => palet[Math.floor(Math.random()*3)];
   
@@ -11,31 +13,38 @@ const Square = ({id, player, newState}) => {
 
   return (
     <button 
-      onClick={(e) => {
+      onClick={e => {
         let col = getRandomColor(); //need to use below
         setColor(col);
-        newState({id:id, color:color});
+        let nextPlayer = newState(id);
+        setStatus(nextPlayer);
         e.target.style.background = col;
       }}
     >
-      <h1>{id}</h1>
+      <h1>{xo[status]}</h1>
     </button>
   );
 };
 
 const Board = () => {
   const [player, setPlayer] = React.useState(1);
-  const [state, setState] = React.useState([]);
+  const [state, setState] = React.useState(Array(9).fill(null));
   //set total game state
   let status = `Player ${player}`;
+  let winner = checkWinner(state);
+  if(winner != null) status =`Player ${winner} wins!`
 
-  const newState = (obj)=>{
-    setState([...state, obj]);
+  const newState = idOfSquare =>{
+    let thePlayer = player;
+    state[idOfSquare] = player; // player is present player
+    setState([state]); // state is array of 0 or 1 or null
+    let nextPlayer = (player + 1) % 2;
     console.log(`adding state ${JSON.stringify(obj)}`);
+    return thePlayer;
   }
 
   function renderSquare(i){
-    return <Square id={i} player={player} newState={newState}> </Square>;
+    return <Square id={i} newState={newState}> </Square>;
   }
   return (
     <div className="game-board">
@@ -44,24 +53,24 @@ const Board = () => {
           {renderSquare(1)}
           {renderSquare(2)}
         </div>
+        <div className="grid-row">
+          {renderSquare(3)}
+          {renderSquare(4)}
+          {renderSquare(5)}
+        </div>
+        <div className="grid-row">
+          {renderSquare(6)}
+          {renderSquare(7)}
+          {renderSquare(8)}
+        </div>
         <div id="info">
           <button>Show/Hide Row</button>
           <button>Re-render</button>
-        <h1>Turn of palyer {player}</h1>
+        <h1>{status}</h1>
       </div>
     </div>
   );
   
-  // ==============================
-  //    onClick={(e) => {
-   //     setPlayer(player ? 0:1);
-  //      status = `Player ${player}`;
-  //      e.target.style.background = "green";
-  //      e.target.style.width = 600;
-  //    }}
-  //  >
-
- // );
 };
 
 // ========================================
