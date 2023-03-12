e575a2c4c28c97fc0703dc62decf9891b6e67319
SamanthaJWheeler <115271437+SamanthaJWheeler@users.noreply.github.com>
2023-03-12 13:19:28 -0400

    Update colorGame00.js

--- colorGame00.js
+++ colorGame00.js
@@ -1,19 +1,45 @@
-const Square = () =>{
+const Square = ({id, player}) =>{
+  const [color, setColor] = React.useState('green');
+  const palet = ["red", "blue", "green"];
+  const getRandomColor = () => palet[Math.floor(Math.random()*3)];
+  React.useEffect(()=> {
+    console.log(`Render ${id}`);
+    return ()=> console.log(`unmounting square ${id}`);
+  })
   return (
-    <button> </button>
-  )
-}
+    <button 
+      onClick={(e) =>{
+        setColor(getRandomColor());
+        e.target.style.background = color;
+        alert(`I'm square ${id}`); 
+      }}
+    >
+    <h1>{player}</h1>
+    </button>
+  );
+};
 
 const Board = () => {
   const [player, setPlayer] = React.useState(0);
+  const [mounted, setMounted] = React.useState(true);
+  const [random, setRandom] = React.useState(0);
   let status = `Player ${player}`;
+  const toggle = () => setMounted(!mounted);
+  const reRender = ()=> setRandom(Math.random());
+
   function renderSquare(i){
-    return <Square> </Square>;
+    return <Square id={i} player={player}> </Square>;
   }
   return (
     <div className="game-board">
-        <div className="grid-row">{renderSquare(0)}</div>
+        <div className="grid-row">
+          {mounted && renderSquare(0)}
+          {mounted && renderSquare(1)}
+          {mounted && renderSquare(2)}
+        </div>
         <div id="info">
+          <button onClick={toggle}>Show/Hide Row</button>
+          <button onClick={reRender}>Re-render</button>
         <h1>{status}</h1>
       </div>
     </div>
